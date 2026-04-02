ba4c5cb4b703539d053bcc6b66acf629ddee0462
ygazzah <83963609+ygazzah@users.noreply.github.com>
2026-04-02 14:32:57 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ Object.assign=require('object-assign')
 
 app.engine('html', require('ejs').renderFile);
 app.use(morgan('combined'))
+app.use(express.static('public'));
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
